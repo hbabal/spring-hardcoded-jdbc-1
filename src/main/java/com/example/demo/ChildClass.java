35c067881c6e5c8cd6d283b3a34e20aedf1a90ed
Huseyin BABAL <huseyin.babal@aurea.com>
2018-01-01 20:35:04 +0300

    New Scenario for RFC v2

--- src/main/java/com/example/demo/ChildClass.java
+++ src/main/java/com/example/demo/ChildClass.java
@@ -3,7 +3,7 @@ package com.example.demo;
 public class ChildClass extends ParentClass {
 
     public void sayHello() {
-        printHello();
+        System.out.println("Hello from child");
     }
 
 }
